70a4bec698bd40b19edd88055ca9ecdd872ba3d2
Diorman Colmenares <dextrin.diorman@gmail.com>
2016-11-28 19:38:21 -0500

    Allow multi docker image build and push

--- gcr-upload.sh
+++ gcr-upload.sh
@@ -3,16 +3,48 @@
 set -e
 
 run-gcr-upload() {
-  local docker_image_name="${1:-$DOCKER_IMAGE_NAME}"
+  local key
+  local build_script=
+  local docker_images=()
+  local docker_image
 
-  if [ -z "$docker_image_name" ]; then echo "docker image name is not specified"; exit 1; fi
+  while [[ $# -gt 0 ]]; do
+    key="$1"
 
-  echo "Building docker image..."
-  docker build -t ${docker_image_name}:${CIRCLE_SHA1} .
-  docker tag ${docker_image_name}:${CIRCLE_SHA1} ${docker_image_name}:latest
+    case $key in
+      -f|--build-file)
+        build_script="$2"
+        shift
+        ;;
 
-  echo "Pushing image to registry..."
-  sudo /opt/google-cloud-sdk/bin/gcloud docker -- push ${docker_image_name}
+      -i|--image)
+        docker_images+=("$2")
+        shift
+        ;;
+    esac
+    shift
+  done
+
+  if [[ ${#docker_images[@]} -eq 0 && -n "$DOCKER_IMAGE_NAME" ]]; then
+    docker_images+=("$DOCKER_IMAGE_NAME")
+  elif [[ ${#docker_images[@]} -eq 0 && -z "$DOCKER_IMAGE_NAME" ]]; then
+    echo "docker images not specified"
+    exit 1
+  fi
+
+  if [[ -n "$build_script" ]]; then
+    $build_script
+  else
+    for docker_image in "${docker_images[@]}"; do
+      echo "Building docker image: $docker_image..."
+      docker build -t ${docker_image}:${CIRCLE_SHA1} .
+    done
+  fi
+
+  for docker_image in "${docker_images[@]}"; do
+    echo "Pushing image $docker_image to registry..."
+    sudo /opt/google-cloud-sdk/bin/gcloud docker -- push ${docker_image}
+  done
 }
 
 run-gcr-upload "$@"
